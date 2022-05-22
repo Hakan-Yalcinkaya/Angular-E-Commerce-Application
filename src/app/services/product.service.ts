7a2db1ea8def0e2e03eda60a98071a292824a71f
hakan yalçınkaya <hakan.yalcinkaya57@gmail.com>
2022-05-22 19:23:50 +0300

    Angular-E-Commerce-Application

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+import { catchError, Observable, tap,throwError } from 'rxjs';
+import { Product } from '../product/product';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+  path="http://localhost:3000/products";
+  getProducts(categoryId):Observable<Product[]>{
+    let newPath=this.path;
+    if(categoryId){
+      newPath+="?categoryId="+categoryId
+    }
+    return this.http
+    .get<Product[]>(newPath).pipe(
+      tap(data=>console.log(JSON.stringify(data))),catchError(this.handleError)
+    );
+
+  }
+  addProduct(product:Product):Observable<Product>{
+    const httpOptions={
+      headers:new HttpHeaders({
+        "Content-Type":"application/json",
+        "Authorization":"Token"
+      })
+    }
+    return this.http.post<Product>(this.path,product,httpOptions).pipe(
+      tap(data=>console.log(JSON.stringify(data))),catchError(this.handleError)
+    );
+  }
+  handleError(err:HttpErrorResponse) {
+    let errorMessage=""
+    if(err.error instanceof ErrorEvent){
+      errorMessage="Bir Hata Oluştu"+err.error.message;
+    }else{
+      errorMessage="Sistemsel Bir Hata";
+    }
+    return throwError(errorMessage);
+  }
+}
